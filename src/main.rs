72283249846ed5a1cd9608c3c04e20a971bd3c80
charlieroth <charlieroth4@gmail.com>
2025-01-10 17:07:10 +0100

    State Machines
    Using a task based approach, with a simple channel implementation,
    the active LED and two buttons can be treated as state machines whose
    state is evaluated on each loop. Based on the actions taken place and
    the current state of the LED changes, the LED lights in the first row
    of the LED matrix will change.

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,53 @@
 #![no_std]
 #![no_main]
 
+mod button;
+mod channel;
+mod led;
 mod time;
 
+use button::{ButtonDirection, ButtonTask};
+use channel::Channel;
 use cortex_m_rt::entry;
-use embedded_hal::{
-    delay::DelayNs,
-    digital::{InputPin, OutputPin, StatefulOutputPin},
-};
-use microbit::{hal::Timer, Board};
+use embedded_hal::digital::OutputPin;
+use led::LedTask;
+use microbit::Board;
 use panic_rtt_target as _;
 use rtt_target::rtt_init_print;
+use time::Ticker;
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     let board = Board::take().unwrap();
-    // Dedicating additional hardware timers will be needed to
-    // track time for each delay the program needs to measure.
-    // This will not scale well.
-    let mut timer = Timer::new(board.TIMER0);
+    let ticker = Ticker::new(board.RTC0);
 
-    let mut left_button = board.buttons.button_a.degrade();
-    let mut right_button = board.buttons.button_b.degrade();
+    let left_button = board.buttons.button_a.degrade();
+    let right_button = board.buttons.button_b.degrade();
 
-    let (mut col, mut row) = board.display_pins.degrade();
+    let (col, mut row) = board.display_pins.degrade();
     row[0].set_high().ok();
 
-    let active_col: usize = 0;
+    let channel: Channel<ButtonDirection> = Channel::new();
+    let mut led_task = LedTask::new(col, &ticker, channel.get_receiver());
+    let mut left_button_task = ButtonTask::new(
+        left_button,
+        &ticker,
+        ButtonDirection::Left,
+        channel.get_sender(),
+    );
+    let mut right_button_task = ButtonTask::new(
+        right_button,
+        &ticker,
+        ButtonDirection::Right,
+        channel.get_sender(),
+    );
+
+    // Create an event loop which polls each task every loop and performs
+    // the appropriate state change based on the states of the tasks.
     loop {
-        col[active_col].toggle().ok();
-        timer.delay_ms(500); // Blocking here
-
-        // Blocking prevents timely detection and response to these
-        if left_button.is_low().unwrap() {
-            // ...
-        }
-
-        if right_button.is_low().unwrap() {
-            // ...
-        }
+        led_task.poll();
+        left_button_task.poll();
+        right_button_task.poll();
     }
 }
